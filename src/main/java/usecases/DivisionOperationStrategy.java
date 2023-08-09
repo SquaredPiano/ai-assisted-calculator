dd22c26771bc948b7f988b54bfcb1ac07733ddca
SquaredPiano <vishnukishan123@gmail.com>
2023-08-09 01:26:31 -0400

    Fixed tests

--- src/main/java/usecases/DivisionOperationStrategy.java
+++ src/main/java/usecases/DivisionOperationStrategy.java
@@ -15,6 +15,9 @@ public class DivisionOperationStrategy extends ArithmeticOperation {
      */
     @Override
     public double execute(double a, double b) {
+        if (b == 0){
+            throw new IllegalArgumentException("Division By Zero");
+        }
         return a / b;
     }
 }
--- src/main/java/usecases/TangentOperationStrategy.java
+++ src/main/java/usecases/TangentOperationStrategy.java
@@ -14,6 +14,6 @@ public class TangentOperationStrategy implements TrigonometricOperation {
      */
     @Override
     public double execute(double angle) {
-        return Math.cos(Math.toRadians(angle));
+        return Math.tan(Math.toRadians(angle));
     }
 }
